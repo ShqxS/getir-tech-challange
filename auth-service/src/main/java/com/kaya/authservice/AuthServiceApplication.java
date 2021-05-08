75eb308da263634577dcf9b889d762acc245fb1b
Caner Kaya <canerky96@gmail.com>
2021-05-08 22:23:10 +0300

    swagger added to auth service.

--- auth-service/src/main/java/com/kaya/authservice/AuthServiceApplication.java
+++ auth-service/src/main/java/com/kaya/authservice/AuthServiceApplication.java
@@ -3,9 +3,11 @@ package com.kaya.authservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableAuthorizationServer
+@EnableSwagger2
 public class AuthServiceApplication {
 
   public static void main(String[] args) {
